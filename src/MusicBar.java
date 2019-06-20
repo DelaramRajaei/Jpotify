fc8762ef87b0386633cd412b598f4f350856353e
DelaramRajaei <rajaeid@aut.ac.ir>
2019-06-20 11:54:21 +0430

    New library

--- src/MusicBar.java
+++ src/MusicBar.java
@@ -4,6 +4,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import javaz
 
 public class MusicBar extends JPanel implements ActionListener  {
     private JButton pause;
@@ -66,7 +67,7 @@ public class MusicBar extends JPanel implements ActionListener  {
          }
     }
 
-        /*public void run(){
+        public void run(){
             FileInputStream fis;
             try{
                 fis=new FileInputStream("03. Smooth Criminal");
@@ -76,7 +77,7 @@ public class MusicBar extends JPanel implements ActionListener  {
                 e.printStackTrace();
             }
 
-        }*/
+        }
 
 
 }
