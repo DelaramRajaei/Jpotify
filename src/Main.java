afa9f12622335aa6ecec3edbf1a4950512f924a6
Darya Zare <daryaz2079@gmail.com>
2019-06-21 18:59:09 +0430

    play awv

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 import javazoom.jl.decoder.JavaLayerException;
 import javazoom.jl.player.Player;
 
+import javax.swing.*;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
@@ -10,7 +11,23 @@ public class Main {
     public static void main(String[] args) {
 
         ArrayList<Music> musics=new ArrayList<Music>();
-        RootPanel rootPanel=new RootPanel();
+       // RootPanel rootPanel=new RootPanel();
+
+
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
+        SwingUtilities.invokeLater(new Runnable() {
+
+            @Override
+            public void run() {
+                new RootPanel().setVisible(true);
+            }
+        });
+
 
     }
 
